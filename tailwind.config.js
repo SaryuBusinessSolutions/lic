790b4387f98455f278cdfb4b5bd4209a60ea9d39
RAHUL1115 <rahulgupta.ra.com@gmail.com>
2022-07-22 16:14:24 +0530

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/{*.ejs,*.html,main.js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors : {
+        'lic-blue': '#004f8f',
+        'lic-yellow': '#ffc908',
+      },
+    },
+  },
+  plugins: [
+  ],
+}
