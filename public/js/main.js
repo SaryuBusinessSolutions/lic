6f7599d65578dc4e9a63de660a5eab6e4052daf1
jai-sanghvi <85071341+jai-sanghvi@users.noreply.github.com>
2022-07-27 17:28:06 +0530

    BUILD - undefined

--- public/js/main.js
+++ public/js/main.js
@@ -1,76 +1,136 @@
-new Splide('.splide', { type: 'loop', autoplay: true, interval: 3000 }).mount();
-
-const menu = document.querySelector('.menu');
-const menuToggle = document.querySelector('.menu-toggle');
-const popup = document.querySelector('.popup');
-const quiz = document.querySelector('.quiz');
-const closeButton = document.querySelector('.close');
+// variables
 var notificationCount = 0;
 var emailNotProcessing = true;
+var showLog = true;
 
-function menuExpand() {
-    menu.classList.toggle("!h-auto");
-    document.querySelector('.bar2').classList.toggle("opacity-0");
-    document.querySelector('.bar1').classList.toggle("-rotate-45");
-    document.querySelector('.bar1').classList.toggle("translate-y-px");
-    document.querySelector('.bar3').classList.toggle("-translate-y-px");
-    document.querySelector('.bar3').classList.toggle("rotate-45");
-}
+var menu, menuToggle;
+var popup, quiz, closeButton;
 
-document.querySelectorAll('.inputs').forEach(e => {
-    e.firstElementChild.addEventListener('change', () => {
-        if (e.classList.contains("correct")) {
-            e.classList.add("bg-green-500", "text-white");
-            setTimeout(() => {
-                let first = e.parentElement.parentElement;
-                first.classList.toggle("hidden");
-                first.nextElementSibling.classList.toggle("hidden");
-            }, 250);
-        } else {
-            e.classList.add("bg-red-500", "text-white");
-            setTimeout(() => {
-                pageReset();
-            }, 250);
-        }
-    });
-});
 
-function pageReset() {
-    if (document.querySelector(".page:first-of-type").classList.contains("hidden")) {
-        document.querySelector(".page:first-of-type").classList.remove("hidden");
-        document.querySelectorAll(".page:not(:first-of-type)").forEach(page => {
-            if (!page.classList.contains("hidden")) {
-                page.classList.add("hidden");
-            }
-        });
-    }
-    document.querySelectorAll('.inputs').forEach(input => {input.firstElementChild.checked=false;})
-    document.querySelectorAll(".page label").forEach(label => {
-        if (label.classList.contains("bg-green-500")) {
-            label.classList.remove("bg-green-500");
-        } else if (label.classList.contains("bg-red-500")) {
-            label.classList.remove("bg-red-500");
-        }
-    });
-}
-
-function popupDisplay() {
-    popup.classList.toggle('hidden');
-    popup.classList.toggle('flex');
-}
-menuToggle.addEventListener('click', menuExpand);
-quiz.addEventListener('click', popupDisplay);
-closeButton.addEventListener('click', popupDisplay);
+// even listener
+// window click
 window.addEventListener('click', (e) => {
     if (e.target == popup) { popupDisplay() };
 });
 
+// document load
 document.addEventListener('DOMContentLoaded', () => {
+    menuInit();
+    popUpInit();
+    splideInit();
     setTimeout(() => {
         popupDisplay();
     }, 2000);
 });
 
+
+// functions
+// menu init
+function menuInit() {
+    try {
+        menu = document.querySelector('.menu');
+        menuToggle = document.querySelector('.menu-toggle');
+
+        menuToggle.addEventListener('click', menuExpand);
+    } catch (error) {
+        if (showLog) console.log(error)
+    }
+
+}
+
+// popup init
+function popUpInit() {
+    try {
+        popup = document.querySelector('.popup');
+        quiz = document.querySelector('.quiz');
+        closeButton = document.querySelector('.close');
+
+        quiz.addEventListener('click', popupDisplay);
+        closeButton.addEventListener('click', popupDisplay);
+
+        document.querySelectorAll('.inputs').forEach(e => {
+            e.firstElementChild.addEventListener('change', () => {
+                if (e.classList.contains("correct")) {
+                    e.classList.add("bg-green-500", "text-white");
+                    setTimeout(() => {
+                        let first = e.parentElement.parentElement;
+                        first.classList.toggle("hidden");
+                        first.nextElementSibling.classList.toggle("hidden");
+                    }, 250);
+                } else {
+                    e.classList.add("bg-red-500", "text-white");
+                    setTimeout(() => {
+                        pageReset();
+                    }, 250);
+                }
+            });
+        });
+    } catch (error) {
+        if (showLog) console.log(error)
+    }
+
+}
+
+// home page splide init
+function splideInit() {
+    try {
+        new Splide('.splide', { type: 'loop', autoplay: true, interval: 3000 }).mount();
+    } catch (error) {
+        if (showLog) console.log(error);
+    }
+}
+
+// to expand menu
+function menuExpand() {
+    try {
+        menu.classList.toggle("!h-auto");
+        document.querySelector('.bar2').classList.toggle("opacity-0");
+        document.querySelector('.bar1').classList.toggle("-rotate-45");
+        document.querySelector('.bar1').classList.toggle("translate-y-px");
+        document.querySelector('.bar3').classList.toggle("-translate-y-px");
+        document.querySelector('.bar3').classList.toggle("rotate-45");
+    } catch (error) {
+        if (showLog) console.log(error);
+    }
+}
+
+// to rest the input when answer is wrong
+function pageReset() {
+    try {
+        if (document.querySelector(".page:first-of-type").classList.contains("hidden")) {
+            document.querySelector(".page:first-of-type").classList.remove("hidden");
+            document.querySelectorAll(".page:not(:first-of-type)").forEach(page => {
+                if (!page.classList.contains("hidden")) {
+                    page.classList.add("hidden");
+                }
+            });
+        }
+        document.querySelectorAll('.inputs').forEach(input => { input.firstElementChild.checked = false; })
+        document.querySelectorAll(".page label").forEach(label => {
+            if (label.classList.contains("bg-green-500")) {
+                label.classList.remove("bg-green-500");
+            } else if (label.classList.contains("bg-red-500")) {
+                label.classList.remove("bg-red-500");
+            }
+        });
+    } catch (error) {
+        if (showLog) console.log(error);
+    }
+}
+
+// Q&A popup init
+function popupDisplay() {
+    try {
+        popup.classList.toggle('hidden');
+        popup.classList.toggle('flex');
+        // popup.classList.toggle(['flex','hidden']); //try this one ek bar upar wala comment kar ke
+    } catch (error) {
+        if (showLog) console.log(error);
+    }
+
+}
+
+// for composibng mail for form
 function composeContactMail(formData) {
     let subject, body;
     subject = formData.has('name') ? "Website Form Submision by " + formData.get('name') : "New Form Submision";
@@ -96,61 +156,70 @@ function composeContactMail(formData) {
     };
 }
 
+// for form submit
 function submitForm(event) {
-    event.preventDefault();
-    let formData = new FormData(event.target);
-    let {
-        subject,
-        body
-    } = composeContactMail(formData);
-    formData.append("subject", subject);
-    formData.append("body", body);
-    event.target.reset();
-    // showNotification("Sending")
-    fetch("https://api.saryuweb.com/email", {
-        method: "POST",
-        body: formData,
-    }).then((response) => {
-        response.json().then((data) => {
-            if (response.status == 401) showNotification("Error occured. Try again.")
-            else showNotification("Form Submitted")
+    try {
+        event.preventDefault();
+        let formData = new FormData(event.target);
+        let {
+            subject,
+            body
+        } = composeContactMail(formData);
+        formData.append("subject", subject);
+        formData.append("body", body);
+        event.target.reset();
+        // showNotification("Sending")
+        fetch("https://api.saryuweb.com/email", {
+            method: "POST",
+            body: formData,
+        }).then((response) => {
+            response.json().then((data) => {
+                if (response.status == 401) showNotification("Error occured. Try again.")
+                else showNotification("Form Submitted")
+            })
+        }).catch((error) => {
+            showNotification('Error occured. Try again.');
         })
-    }).catch((error) => {
-        showNotification('Error occured. Try again.');
-    })
+    } catch (error) {
+        if (showLog) console.log(error);
+    }
 }
 
-
+// for notification
 function showNotification(text) {
-    let newNode = document.createElement('div');
-    newNode.id = "alert" + notificationCount;
-    newNode.className = "p-3 px-4 flex items-center space-x-4 rounded-xl bg-green-500 transition-all duration-300 pointer-events-auto";
-    notificationCount++;
-    let childNode1 = document.createElement('p')
-    childNode1.className = 'w-52 text-sm';
-    childNode1.innerText = text;
-    let childNode2 = document.createElement('i')
-    childNode2.className = 'fa-solid fa-xmark cursor-pointer';
-    newNode.appendChild(childNode1);
-    newNode.appendChild(childNode2);
+    try {
+        let newNode = document.createElement('div');
+        newNode.id = "alert" + notificationCount;
+        newNode.className = "p-3 px-4 flex items-center space-x-4 rounded-xl bg-green-500 transition-all duration-300 pointer-events-auto";
+        notificationCount++;
+        let childNode1 = document.createElement('p')
+        childNode1.className = 'w-52 text-sm';
+        childNode1.innerText = text;
+        let childNode2 = document.createElement('i')
+        childNode2.className = 'fa-solid fa-xmark cursor-pointer';
+        newNode.appendChild(childNode1);
+        newNode.appendChild(childNode2);
 
-    let domAlert = document.querySelector("#alert");
-    domAlert.appendChild(newNode);
+        let domAlert = document.querySelector("#alert");
+        domAlert.appendChild(newNode);
 
-    let opacityTimeOut = setTimeout(() => {
-        newNode.classList.add("opacity-0")
-    }, 4700);
+        let opacityTimeOut = setTimeout(() => {
+            newNode.classList.add("opacity-0")
+        }, 4700);
 
-    let removeTimeOut = setTimeout(() => {
-        newNode.remove();
-    }, 5000);
-
-    childNode2.onclick = () => {
-        clearTimeout(opacityTimeOut)
-        clearTimeout(removeTimeOut)
-        newNode.classList.add("opacity-0")
-        setTimeout(() => {
+        let removeTimeOut = setTimeout(() => {
             newNode.remove();
-        }, 300);
+        }, 5000);
+
+        childNode2.onclick = () => {
+            clearTimeout(opacityTimeOut)
+            clearTimeout(removeTimeOut)
+            newNode.classList.add("opacity-0")
+            setTimeout(() => {
+                newNode.remove();
+            }, 300);
+        }
+    } catch (error) {
+        if (showLog) console.log(error)
     }
 }
